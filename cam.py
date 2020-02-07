47649c00c51ebb93cadf352e7d210f74c3c016a0
svmihar <svmihar@gmail.com>
2020-02-07 09:02:56 +0700

    :art: better code structure

--- cam.py
+++ cam.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import argparse
 import logging
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
@@ -10,33 +11,41 @@ logger = logging.getLogger(__name__)
 
 cap=cv2.VideoCapture(0)
 
+def main(thresholdarea: int, delaytime: int):
+    while(True):
+        ret1,frame1= cap.read()
+        gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+        gray1 = cv2.GaussianBlur(gray1, (21, 21), 0)
+        ret2,frame2=cap.read()
+        cv2.waitKey(delaytime)
+        gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+        gray2 = cv2.GaussianBlur(gray2, (21, 21), 0)
 
-while(True):
-    ret1,frame1= cap.read()
-    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    gray1 = cv2.GaussianBlur(gray1, (21, 21), 0)
-    ret2,frame2=cap.read()
-    cv2.waitKey(100)
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.GaussianBlur(gray2, (21, 21), 0)
-    
-    deltaframe=cv2.absdiff(gray1,gray2)
-    cv2.imshow('delta',deltaframe)
-    threshold = cv2.threshold(deltaframe, 25, 255, cv2.THRESH_BINARY)[1]
-    threshold = cv2.dilate(threshold,np.ones((2,2), np.uint8),iterations=2)
-    cv2.imshow('threshold',threshold)
-    _,countour,heirarchy = cv2.findContours(threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    for i in countour:
-        if cv2.contourArea(i) <500 :
-            text = "isi"
-            logger.info(cv2.contourArea(i))
-            continue
-        (x, y, w, h) = cv2.boundingRect(i)
-        cv2.rectangle(frame2, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    cv2.putText(frame2, f'Status: {text}', (10,40), cv2.FONT_HERSHEY_SIMPLEX, 2.1, (0, 0, 255), 4)
-    cv2.imshow('window',frame2)
-    text = "kosong"
-    if cv2.waitKey(20) == ord('q'):
-      break
-cap.release()
-cv2.destroyAllWindows()
+        deltaframe=cv2.absdiff(gray1,gray2)
+        cv2.imshow('delta',deltaframe)
+        threshold = cv2.threshold(deltaframe, 25, 255, cv2.THRESH_BINARY)[1]
+        threshold = cv2.dilate(threshold,np.ones((3,3), np.uint8),iterations=2)
+        cv2.imshow('threshold',threshold)
+        _,countour,heirarchy = cv2.findContours(threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for i in countour:
+            if cv2.contourArea(i) <thresholdarea :
+                text = "isi"
+                logger.info(cv2.contourArea(i))
+                continue
+            (x, y, w, h) = cv2.boundingRect(i)
+            cv2.rectangle(frame2, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(frame2, f'Status: {text}', (10,40), cv2.FONT_HERSHEY_SIMPLEX, 2.1, (0, 0, 255), 5)
+        cv2.imshow('window',frame2)
+        text = "kosong"
+        if cv2.waitKey(20) == ord('q'):
+          break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--threshold', type=int, default=500)
+    parser.add_argument('--delay', type=int, default=100)
+    args = vars(parser.parse_args())
+    main(**args)
--- readme.md
+++ readme.md
@@ -1,4 +1,11 @@
-# requirements: 
-`pip install -r requirements.txt`    
-or 
+# requirements:
+`pip install -r requirements.txt`
+or
 `conda env create --file=enviroment.yml`
+# how to
+`python cam.py`
+
+```
+--threshold [int]  how big the area of difference? (DEFAULT: 500)
+--delay 0>[int]>100, the bigger the slower the fps (DEFAULT: 100)
+```
